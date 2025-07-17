32eea231242621a6cee038fc29d6c050e1cf2a97
Yavor Lalev <yavorlalev@abv.bg>
2025-07-17 11:58:31 +0200

    updated-styling

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -5,14 +5,21 @@ export default function LandingPage() {
   return (
     <main className="flex items-center p-10 gap-24 animate-fade-in max-md:flex-col">
       <section className="flex flex-col items-center">
-        <Image src="/assets/logo.svg" width={200} height={200} alt="logo" />
+        <Image src="/assets/logo.svg" width={300} height={300} alt="logo" />
         <h1 className="text-2x1 font-black lg:text-3x1">Friday on my mind</h1>
         <p className="font-extralight">
           Book your meetings and don't forget about Friday
         </p>
-        <Image src="/assets/planning.svg" width={300} height={300} alt="Logo" />
+        <div className="mt-10">
+          <Image
+            src="/assets/planning.svg"
+            width={500}
+            height={500}
+            alt="Logo"
+          />
+        </div>
       </section>
-      {/* Clerk Sign-In Component with custom theme */}
+      {/* Clerk Sign-In Component*/}
       <div className="mt-3">
         <SignIn
           routing="hash" //Keeps sign-in UI on the same page using hash-based routing
