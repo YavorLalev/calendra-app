fdacac81f06decc72bbb02015bcc2e84904dc7de
Yavor Lalev <yavorlalev@abv.bg>
2025-07-27 14:43:05 +0200

    added-SignUp-and-SignIn-buttons

--- components/PublicNavBar.tsx
+++ components/PublicNavBar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { SignInButton, SignUp, SignUpButton } from "@clerk/nextjs";
 export default function PublicNavBar() {
   return (
     <nav className="flex justify-between items-center fixed z-50 w-full h-20 bg-amber-200 px-10 gap-3">
@@ -9,6 +10,16 @@ export default function PublicNavBar() {
       >
         <Image src="/assets/Logo.svg" width={60} height={60} alt="Logo" />
       </Link>
+      <section className="sticky top-0 flex justify-between">
+        <div className="flex flex-1 max-sm:gap-0 sm:gap-6">
+          <SignInButton>
+            <button>Login</button>
+          </SignInButton>
+          <SignUpButton>
+            <button>Register</button>
+          </SignUpButton>
+        </div>
+      </section>
     </nav>
   );
 }
